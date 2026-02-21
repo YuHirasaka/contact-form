07ca56e44cc574751a12a6ff08b4a3e13abbbca1
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-02-21 11:56:11 +0900

    feat: Contactモデルにキーワード検索用scopeKeywordSearchを追加

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -32,6 +32,31 @@ class Contact extends Model
             1 => '男性',
             2 => '女性',
             3 => 'その他',
-        ][$this->gender] ?? '' ;
+        ][$this->gender] ?? '';
+    }
+
+    public function scopeKeywordSearch($query, $keyword)
+    {
+        $keyword = trim((string) $keyword);
+        if ($keyword === '') return $query;
+
+        $isExact = preg_match('/^"(.*)"$/u', $keyword, $m) === 1;
+        $term = $isExact ? $m[1] : $keyword;
+
+        $termNoSpace = str_replace(['', ' '], '', $term);
+
+        return $query->where(function ($q) use ($isExact, $term, $termNoSpace) {
+            if ($isExact) {
+                $q->where('last_name', $term)
+                ->orWhere('first_name', $term)
+                ->orWhere('email', $term)
+                ->orWhereRaw("CONCAT(last_name, first_name) = ?", [$termNoSpace]);
+            } else {
+                $q->where('last_name', 'LIKE', "%{$term}%")
+                ->orWhere('first_name', 'LIKE', "%{$term}%")
+                ->orWhere('email', 'LIKE', "%{$term}%")
+                ->orWhereRaw("CONCAT(last_name, first_name) LIKE ?", ["%{$termNoSpace}%"]);
+            }
+        });
     }
 }
